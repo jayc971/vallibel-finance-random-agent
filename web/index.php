031f36256060cf63093feef47a76c4390e802c17
jayc971 <jayanika@serviceform.com>
2021-01-29 00:14:33 +0530

    strategycall CSS

--- web/index.php
+++ web/index.php
@@ -5,5 +5,5 @@ include 'Router.php';
 $request = $_SERVER['REQUEST_URI'];
 $router = new Router($request);
 
-$router->get('/', 'app/home');
+$router->get('/', 'app/index');
 $router->get('post', 'app/post');
