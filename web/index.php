851da63cf5876d5e26cecc1ab00bd3e5aa2b708e
jayc971 <jayanika@serviceform.com>
2021-01-29 00:02:21 +0530

    strategycall CSS

--- web/index.php
+++ web/index.php
@@ -1,79 +1,9 @@
 <?php
 
-/////////////////////////////////////////////////////////////////////
-// This is the only portion of the code you may need to change.
-// Indicate the location of your images 
-$root = '';
-// use if specifying path from root
-//$root = $_SERVER['DOCUMENT_ROOT'];
+include 'Router.php';
 
-$path = 'images/';
+$request = $_SERVER['REQUEST_URI'];
+$router = new Router($request);
 
-// End of user modified section 
-/////////////////////////////////////////////////////////////////////
- 
-function getImagesFromDir($path) {
-    $images = array();
-    if ( $img_dir = @opendir($path) ) {
-        while ( false !== ($img_file = readdir($img_dir)) ) {
-            // checks for gif, jpg, png
-            if ( preg_match("/(\.gif|\.jpg|\.png)$/", $img_file) ) {
-                $images[] = $img_file;
-            }
-        }
-        closedir($img_dir);
-    }
-    return $images;
-}
-
-function getRandomFromArray($ar) {
-    mt_srand( (double)microtime() * 1000000 ); // php 4.2+ not needed
-    $num = array_rand($ar);
-    return $ar[$num];
-}
-
-
-// Obtain list of images from directory 
-$imgList = getImagesFromDir($root . $path);
-
-$img = getRandomFromArray($imgList);
-?> 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8" />
-<title>Demo</title>
-<style type="text/css">
-body { font: 14px/1.3 verdana, arial, helvetica, sans-serif; }
-h1 { font-size:1.3em; }
-h2 { font-size:1.2em; }
-a:link { color:#33c; } 
-a:visited { color:#339; }
-p { max-width: 60em; }
-
-/* so linked image won't have border */
-a img { border:none; }
-</style>
-</head>
-<body>
-
-<?php
-  
-
-    function Redirect($url, $permanent = false)
-{
-    if (headers_sent() === false)
-    {
-        header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
-    }
-
-    exit();
-}
-
-Redirect($path . $img, false);
-?>
-
-
-
-</body>
-</html>
+$router->get('/', 'app/home');
+$router->get('post', 'app/post');
